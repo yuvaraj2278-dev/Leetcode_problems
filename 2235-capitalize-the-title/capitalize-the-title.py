467c051d6d47b61f5d29ae3893757fbf3aa413fa
yuvaraj-dev <yuvaraj2278@gmail.com>
2026-08-18 17:59:13 +0530

    Time: 2 ms (29.45%) | Memory: 12.5 MB (17.82%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.py
+++ 2235-capitalize-the-title/capitalize-the-title.py
@@ -1,9 +1,13 @@
-class Solution:
-    def capitalizeTitle(self, title: str) -> str:
+class Solution(object):
+    def capitalizeTitle(self, title):
+        """
+        :type title: str
+        :rtype: str
+        """
         words = title.split()
         for i in range(len(words)):
             if len(words[i]) <= 2:
                 words[i] = words[i].lower()
             else:
                 words[i] = words[i].capitalize()
-        return " ".join(words)           
+        return " ".join(words)
